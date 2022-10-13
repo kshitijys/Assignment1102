553058b0dcbb99f5af01ad2a5717c6d7a8f807fd
kshitij shelar <kshititjshelar989@gmail.com>
2022-10-13 15:58:24 +0530

    Add project files.

---
+++ Assignment1102/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1102
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("kshitij", 80, 81, 92);
+            s1.calculation();
+            Console.WriteLine(s1);
+
+            Student s2 = new Student("ram", 81, 82, 83);
+            s2.calculation();
+            Console.WriteLine(s2);
+
+            Student s3 = new Student("om", 82, 83, 84);
+            s3.calculation();
+            Console.WriteLine(s3);
+        }
+    }
+}
---
+++ Assignment1102/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1102
+{
+    public class Student
+    {
+        
+        private int rollno,phy,chem,maths,total,percentage;
+        private string name;
+        public static int count;
+
+        public Student(string name, int phy, int chem, int maths)
+        {
+            count++;
+            this.rollno= count;
+            this.name = name;
+            this.phy = phy;
+            this.chem = chem;
+            this.maths = maths;
+            
+        }  
+        public void calculation()
+        {
+            total = phy + chem + maths;
+            percentage = total *100 / 300;
+        }
+
+        public override string ToString()
+        {
+            return $"rollno={rollno} name={name} percentage={percentage}";     
+        }
+
+    }
+}
+//Create class student
+//Auto generate the rollno
+//Accept name & 3 subject marks from constructor
+
+//Use calculate method to calculate total & percentage
+
+//Display details using ToString()
